888aaab69dea59326e663d334452c83bd1fdb522
quoctm <quoctm@vng.com.vn>
2025-03-01 23:48:56 +0700

    add authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
+import {Amplify} from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import {Authenticator} from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Authenticator>
+            <App/>
+        </Authenticator>
+    </React.StrictMode>
 );
